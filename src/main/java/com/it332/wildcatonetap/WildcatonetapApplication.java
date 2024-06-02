0613b3747702c0c30752ce4732eb29231520bc92
Dawn Marie D. Gumagay <143795204+dawnmarie-gumagay@users.noreply.github.com>
2024-06-03 01:53:35 +0800

    Add files via upload

---
+++ src/main/java/com/it332/wildcatonetap/WildcatonetapApplication.java
@@ -0,0 +1,14 @@
+package com.it332.wildcatonetap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WildcatonetapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WildcatonetapApplication.class, args);
+		System.out.println("Ok");
+	}
+
+}
---
+++ src/test/java/com/it332/wildcatonetap/WildcatonetapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.it332.wildcatonetap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WildcatonetapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
