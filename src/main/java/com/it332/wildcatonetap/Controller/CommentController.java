e728625471a97ce8f2036b0aea927cf1ec447f94
Dawn Marie D. Gumagay <dawnmarie.gumagay@cit.edu>
2024-07-11 05:13:29 +0800

    Updated the CommentController
    I added the DeleteComment

--- src/main/java/com/it332/wildcatonetap/Controller/CommentController.java
+++ src/main/java/com/it332/wildcatonetap/Controller/CommentController.java
@@ -3,11 +3,15 @@ package com.it332.wildcatonetap.Controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.it332.wildcatonetap.Entity.CommentEntity;
@@ -15,6 +19,7 @@ import com.it332.wildcatonetap.Service.CommentService;
 
 @RestController
 @RequestMapping("/comments")
+@CrossOrigin(origins = "http://localhost:3000")
 public class CommentController {
 
     @Autowired
@@ -28,5 +33,15 @@ public class CommentController {
     @PostMapping("/add")
     public CommentEntity addComment(@RequestBody CommentEntity comment) {
         return commentService.addComment(comment);
+    }
+
+    @DeleteMapping("/{commentId}")
+    public ResponseEntity<?> deleteComment(@PathVariable int commentId, @RequestParam int userId) {
+        boolean deleted = commentService.deleteComment(commentId, userId);
+        if (deleted) {
+            return ResponseEntity.ok().build();
+        } else {
+            return ResponseEntity.badRequest().body("Unable to delete comment. You may not have permission.");
+        }
     }
 }
