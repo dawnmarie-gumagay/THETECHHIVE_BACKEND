73de1129bc343f49212df96270c74e3edc25c9ab
Dawn Marie D. Gumagay <dawnmarie.gumagay@cit.edu>
2024-07-13 16:01:16 +0800

    Updated the PostEntity
    I Removed some of the imports and I added the localdatetime import

--- src/main/java/com/it332/wildcatonetap/Entity/PostEntity.java
+++ src/main/java/com/it332/wildcatonetap/Entity/PostEntity.java
@@ -1,11 +1,7 @@
 package com.it332.wildcatonetap.Entity;
 
-import java.sql.Date;
+
 import java.time.LocalDateTime;
-import java.time.LocalTime;
-
-import org.springframework.cglib.core.Local;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
