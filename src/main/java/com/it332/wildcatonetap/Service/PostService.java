250ab3b075783a9ffcb094cb22b3be10503c8b71
Dawn Marie D. Gumagay <dawnmarie.gumagay@cit.edu>
2024-07-13 16:02:52 +0800

    Updated the PostService
    I removed one of the imports

--- src/main/java/com/it332/wildcatonetap/Service/PostService.java
+++ src/main/java/com/it332/wildcatonetap/Service/PostService.java
@@ -5,9 +5,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cglib.core.Local;
 import org.springframework.stereotype.Service;
-
 import com.it332.wildcatonetap.Entity.CommentEntity;
 import com.it332.wildcatonetap.Entity.PostEntity;
 import com.it332.wildcatonetap.Entity.UserEntity;
