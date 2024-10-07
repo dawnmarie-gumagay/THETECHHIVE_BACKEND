13ec776217cb229ec357421d708bbd7a969e02b3
Dawn Marie D. Gumagay <143795204+dawnmarie-gumagay@users.noreply.github.com>
2024-10-08 01:27:48 +0800

    Updated File: WildcatonetapApplication.java
    Summary:
    The WildcatonetapApplication class serves as the entry point for the Wildcat One Tap application. It initializes the Spring Boot application, enabling the application to run.
    
    Key Method:
    main(String[] args): This method is the application's starting point. It invokes SpringApplication.run to launch the application and prints "LABAN CAPSTONE" to the console upon startup.
    
    WALA NI NA HILABTAN NASAYOP RAKO EDIT BUT AKO RA GI BALIK SA ORIGINAL.

--- src/main/java/com/it332/wildcatonetap/WildcatonetapApplication.java
+++ src/main/java/com/it332/wildcatonetap/WildcatonetapApplication.java
@@ -6,9 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WildcatonetapApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WildcatonetapApplication.class, args);
-		System.out.println("LABAN CAPSTONE");
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(WildcatonetapApplication.class, args);
+        System.out.println("LABAN CAPSTONE");
+    }
 }
